6bb6229f1bdcf49e23a8f96f22a777a85bcd4d7f
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-11-17 23:40:27 -0500

    new Project copy from old repository because a created the wrong one

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+//
+
+// this added current time on top ("MMM Do YYY" abbreviation) (MMMM Do("" o"add th) YYYY) full date )
+var currentDay = moment().format("MMMM Do YYYY");
+$("#currentDay").text(currentDay);
+
+//change color relative to now
+var currentColor = $(".hour");
+var now = parseInt(moment().format("H"));
+
+//function to check each hour block to see if it is past (gray), present (red), or future (green).
+$.each(currentColor, function (i, hour) {
+  var hourGap = parseInt($(this).attr("id"));
+  if (hourGap === now) {
+    $(this).next().addClass("present");
+  } else if (hourGap < now) {
+    $(this).next().addClass("past");
+  } else if (hourGap > now) {
+    $(this).next().addClass("future");
+  }
+});
+
+//color-code each time-block< need make changes here
+
+$(".saveBtn").on("click", function (event) {
+  var calendarItem =
+    event.target.parentElement.previousElementSibling.children[0].value;
+  localStorage.setItem(event.target.attributes[0].value, calendarItem);
+});
+
+$(document).ready(function () {
+  if (localStorage["9am"] !== null && localStorage["9am"] !== undefined) {
+    var nineAm = $("<p>" + localStorage["9am"] + "</p>");
+    $("#nineAm").append(nineAm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["10am"] !== null && localStorage["10am"] !== undefined) {
+    var tenAm = $("<p>" + localStorage["10am"] + "</p>");
+    $("#tenAm").append(tenAm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["11am"] !== null && localStorage["11am"] !== undefined) {
+    var elevenAm = $("<p>" + localStorage["11am"] + "</p>");
+    $("#elevenAm").append(elevenAm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["12pm"] !== null && localStorage["12pm"] !== undefined) {
+    var twelvePm = $("<p>" + localStorage["12pm"] + "</p>");
+    $("#twelvePm").append(twelvePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["1pm"] !== null && localStorage["1pm"] !== undefined) {
+    var onePm = $("<p>" + localStorage["1pm"] + "</p>");
+    $("#onePm").append(onePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["2pm"] !== null && localStorage["2pm"] !== undefined) {
+    var twoPm = $("<p>" + localStorage["2pm"] + "</p>");
+    $("#twoPm").append(twoPm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["3pm"] !== null && localStorage["3pm"] !== undefined) {
+    var threePm = $("<p>" + localStorage["3pm"] + "</p>");
+    $("#threePm").append(threePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["4pm"] !== null && localStorage["4pm"] !== undefined) {
+    var fourPm = $("<p>" + localStorage["4pm"] + "</p>");
+    $("#fourPm").append(fourPm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["5pm"] !== null && localStorage["5pm"] !== undefined) {
+    var fivePm = $("<p>" + localStorage["5pm"] + "</p>");
+    $("#fivePm").append(fivePm[0].innerText);
+  } else {
+    ("");
+  }
+});
