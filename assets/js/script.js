a35e4af3f81eb7f661c153d7fa87ade2e584cf60
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-11-21 09:35:00 -0500

    change planworkday to array Thank Charles  TA

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,19 @@ var today = moment().format("dddd, MMMM Do");
 var now = moment().format("H A");
 
 /* planWorkday entries for each hour of the workday */
-var planWorkday = [
-  { time: "9 AM", event: "" },
-  { time: "10 AM", event: "" },
-  { time: "11 AM", event: "" },
-  { time: "12 PM", event: "" },
-  { time: "1 PM", event: "" },
-  { time: "2 PM", event: "" },
-  { time: "3 PM", event: "" },
-  { time: "4 PM", event: "" },
-  { time: "5 PM", event: "" },
-];
+// var planWorkday = [
+//   { time: "9 AM", event: "" },
+//   { time: "10 AM", event: "" },
+//   { time: "11 AM", event: "" },
+//   { time: "12 PM", event: "" },
+//   { time: "1 PM", event: "" },
+//   { time: "2 PM", event: "" },
+//   { time: "3 PM", event: "" },
+//   { time: "4 PM", event: "" },
+//   { time: "5 PM", event: "" },
+// ];
+
+var planWorkday = [9,10,11, 12, 13, 14, 16, 17]
 
 /* Local Storage check */
 var workEvents = JSON.parse(localStorage.getItem("workDay"));
