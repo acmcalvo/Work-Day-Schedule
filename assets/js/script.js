e63c5bd89b0e90e184f0aac6e6c1b321b09b667f
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-11-21 10:06:10 -0500

    change colorRow function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,35 @@
 $(function () {});
   
-/* Declare Variables */
+// Variables
 var today = moment().format("dddd, MMMM Do");
-
 var now = moment().format("H A");
+var hoursArray = [9,10,11, 12, 13, 14, 16, 17]
 
-/* planWorkday entries for each hour of the workday */
-// var planWorkday = [
-//   { time: "9 AM", event: "" },
-//   { time: "10 AM", event: "" },
-//   { time: "11 AM", event: "" },
-//   { time: "12 PM", event: "" },
-//   { time: "1 PM", event: "" },
-//   { time: "2 PM", event: "" },
-//   { time: "3 PM", event: "" },
-//   { time: "4 PM", event: "" },
-//   { time: "5 PM", event: "" },
-// ];
-
-var planWorkday = [9,10,11, 12, 13, 14, 16, 17]
-
-/* Local Storage check */
+// Local storage JSON
 var workEvents = JSON.parse(localStorage.getItem("workDay"));
 if (workEvents) {
-  planWorkday = workEvents;
+  hoursArray = workEvents;
 }
 
-/* Current Day */
+
+// Get current day
 $("#currentDay").text(today);
 
-/* Create rows */
-planWorkday.forEach(function(timeBlock, index) {
+// add color
+hoursArray.forEach(function(timeBlock, index) {
 	var timeLabel = timeBlock.time;
 	var blockColor = colorRow(timeLabel);
-	var row =
-		'<div class="time-block" id="' +
-		index +
-		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-		timeLabel +
-		'</div><textarea class="form-control ' +
-		blockColor +
-		'">' +
-		timeBlock.event +
-		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+	var row ='<div class="time-block" id="' + 
+	index +'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+	timeLabel +'</div><textarea class="form-control ' +
+	blockColor + '">' +
+	timeBlock.event +'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
 
-	/* Adding rows to container div */
+	
 	$(".container").append(row);
 });
 
-/* Color rows based on current time */
+// base in time add colors
 function colorRow(time) {
 	var planNow = moment(now, "H A");
 	var planEntry = moment(time, "H A");
@@ -61,7 +42,7 @@ function colorRow(time) {
 	}
 }
 
-/* Save Events */
+// events save
 $(".saveBtn").on("click", function() {
 	var blockID = parseInt(
 		$(this)
@@ -74,8 +55,8 @@ $(".saveBtn").on("click", function() {
 			.siblings("textarea")
 			.val()
 	);
-	planWorkday[blockID].event = userEntry;
+	hoursArray[blockID].event = userEntry;
 
-	/* Set local storage */
-	localStorage.setItem("workDay", JSON.stringify(planWorkday));
+	// Setup localStorage
+	localStorage.setItem("workDay", JSON.stringify(hoursArray));
 });
